9daf8a86ce4f425870a6e778f13151897987f79b
Troy Kauffman <tkauffman@midmark.com>
2026-04-09 21:20:51 -0400

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/masters-web/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
